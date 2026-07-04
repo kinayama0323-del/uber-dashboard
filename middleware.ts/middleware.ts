67c11f6165caf6b6e5100fa5a3cfa7e6df712af6
Kenjiro Inayama <kinayama0323@gmail.com>
2026-07-05 08:56:09 +0900

    test middleware

--- middleware.ts/middleware.ts
+++ middleware.ts/middleware.ts
@@ -11,25 +11,8 @@ export function middleware(req: NextRequest) {
     return NextResponse.next();
   }
 
-  const auth = req.headers.get("authorization");
-
-  if (auth) {
-    const [, encoded] = auth.split(" ");
-    const [user, pass] = atob(encoded).split(":");
-
-    if (
-      user === process.env.ADMIN_USER &&
-      pass === process.env.ADMIN_PASSWORD
-    ) {
-      return NextResponse.next();
-    }
-  }
-
-  return new NextResponse("Authentication required", {
-    status: 401,
-    headers: {
-      "WWW-Authenticate": 'Basic realm="Admin Area"',
-    },
+  return new NextResponse("middleware works", {
+    status: 200,
   });
 }
 
